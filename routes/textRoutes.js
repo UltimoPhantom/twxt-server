2a0f0892f380569966b380830f7e60b7171f86a1
Tejas-Subramanya-K-R <tejas@louisa.ai>
2025-05-22 01:30:25 +0530

    test 2

--- routes/textRoutes.js
+++ routes/textRoutes.js
@@ -8,6 +8,6 @@ import {
 
 router.post('/', createText);
 router.get('/', getAllTexts);
-router.patch('/archive/:uuid', archiveText); //test commit
+router.patch('/archive/:uuid', archiveText);
 
 export default router;
