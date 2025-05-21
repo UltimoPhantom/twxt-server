6bd90c63b8c53bace331465e13275e9fd5e267c4
Tejas-Subramanya-K-R <tejas@louisa.ai>
2025-05-22 00:04:20 +0530

    added linting

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(port, () => console.log(`Server running on port ${port}`));
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
