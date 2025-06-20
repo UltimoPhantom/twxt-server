3ac1cfd0093da20ff514ac391447535730616f6d
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2025-06-21 01:55:09 +0530

    fix: update CORS configuration to allow all origins and methods

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ import userRoutes from './routes/userRoutes.js';
 
 export function createApp() {
   const app = express();
-  const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000';
 
   app.use(cors({
-    origin: clientUrl,
-    credentials: true
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
   app.use(json());
